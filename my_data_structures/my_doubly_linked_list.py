13df8246b4388a1f4450a163d589fc6e6f08e39b
vovapasko <vovapasko1699@gmail.com>
2021-11-21 18:39:15 +0100

    Started implementing doubly linked list

--- my_data_structures/my_doubly_linked_list.py
+++ my_data_structures/my_doubly_linked_list.py
@@ -1,6 +1,15 @@
 from my_doubly_node import MyDoublyNode
 
 class MyDoublyLinkedList:
-    def __init__(self, first, last) -> None:
+    def __init__(self, first= None, last = None) -> None:
         self.first = first
         self.last = last
+
+    def insert(self, node: MyDoublyNode) -> None:
+        if self.first is None:
+            self.first = node
+            self.last = node
+        else:
+            self.first.next = self.last
+            node.previous = self.last
+            self.last.next = node
