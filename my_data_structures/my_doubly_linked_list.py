88f3c747eeda09af7cd57c3d8dfce1b3191cd001
vovapasko <vovapasko1699@gmail.com>
2021-11-22 16:32:58 +0100

    implemented basic doubly linked list

--- my_data_structures/my_doubly_linked_list.py
+++ my_data_structures/my_doubly_linked_list.py
@@ -1,15 +1,58 @@
 from my_doubly_node import MyDoublyNode
 
 class MyDoublyLinkedList:
-    def __init__(self, first= None, last = None) -> None:
+    def __init__(self, first: MyDoublyNode) -> None:
         self.first = first
-        self.last = last
+        self.last = first
 
     def insert(self, node: MyDoublyNode) -> None:
-        if self.first is None:
-            self.first = node
+        if self.first == self.last:
+            self.first.next = node
             self.last = node
+            self.last.previous = self.first
         else:
-            self.first.next = self.last
-            node.previous = self.last
-            self.last.next = node
+            tmp = self.last
+            tmp.next = node
+            node.previous = tmp
+            self.last = node 
+
+    def delete(self, index: int):
+        el_to_delete: MyDoublyNode = self.__iterate_until(index)
+        prev = el_to_delete.previous
+        nxt = el_to_delete.next
+        prev.next = nxt
+        nxt.prerious = prev
+
+    def __iterate_until(self, index):
+        i = 0
+        current: MyDoublyNode = self.first
+        while i < index:
+            next_el = current.next
+            if next_el is None:
+                raise IndexError()
+            current = next_el
+            i += 1
+        return current
+
+    def print(self):
+        current = self.first
+        while current.next is not None:
+            print(str(current.value) + " -> ",end=" ")
+            nxt = current.next
+            current = nxt
+        print(str(current.value))
+
+n0 = MyDoublyNode(0)
+n1 = MyDoublyNode(1)
+n2 = MyDoublyNode(2)
+n3 = MyDoublyNode(3)
+n4 = MyDoublyNode(4)
+
+dl = MyDoublyLinkedList(n0)
+dl.insert(n1)
+dl.insert(n2)
+dl.insert(n3)
+dl.insert(n4)
+dl.print()
+dl.delete(3)
+dl.print()
