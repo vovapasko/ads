f684f7fab623e448523c469efed65fb390921f26
vovapasko <vovapasko1699@gmail.com>
2021-11-25 12:47:46 +0100

    provided inserting of an element in BST

---
+++ my_data_structures/my_bst.py
@@ -0,0 +1,47 @@
+from my_tree_node import MyTreeNode
+from typing import Union
+
+
+class MyBinarySearchTree:
+    def __init__(self) -> None:
+        pass
+
+    def search(self, node: MyTreeNode, value: object) -> Union[MyTreeNode, None]:
+        if node.left is None and node.right is None and node.value != value:
+            return None
+        if node.value == value:
+            return node
+        elif value > node.value:
+            self.search(node.right)
+        elif value < node.value:
+            self.search(node.left)
+
+    def insert(self, tree_node: MyTreeNode, node: MyTreeNode) -> None:
+        if tree_node.value < node.value:
+            if tree_node.right is None:
+                tree_node.right = node
+            else:
+                self.insert(tree_node.right, node)
+        elif tree_node.value > node.value:
+            if tree_node.left is None:
+                tree_node.left = node
+            else:
+                self.insert(tree_node.left, node)
+
+    def delete(self, value) -> Union[MyTreeNode, None]:
+        pass
+
+    def traverse(self) -> None:
+        pass
+
+
+root = MyTreeNode(1)
+n1 = MyTreeNode(5)
+n2 = MyTreeNode(3)
+n3 = MyTreeNode(10)
+n4 = MyTreeNode(6)
+tree = MyBinarySearchTree()
+tree.insert(root, n1)
+tree.insert(root, n2)
+tree.insert(root, n3)
+tree.insert(root, n4)
---
+++ my_data_structures/my_tree_node.py
@@ -0,0 +1,5 @@
+class MyTreeNode:
+    def __init__(self, value: object) -> None:
+        self.value = value
+        self.left = None
+        self.right = None
