453bf5f54566dbce05fabcea0812d8623066b2da
vovapasko <vovapasko1699@gmail.com>
2021-11-25 13:23:49 +0100

    Implemented inorder, preorder and postorder traversing

--- my_data_structures/my_bst.py
+++ my_data_structures/my_bst.py
@@ -21,28 +21,67 @@ class MyBinarySearchTree:
             if tree_node.right is None:
                 tree_node.right = node
             else:
-                return self.insert(tree_node.right, node)
+                self.insert(tree_node.right, node)
         elif tree_node.value > node.value:
             if tree_node.left is None:
                 tree_node.left = node
             else:
-                return self.insert(tree_node.left, node)
+                self.insert(tree_node.left, node)
 
-    def delete(self, value) -> Union[MyTreeNode, None]:
+    def delete(self, tree_node: MyTreeNode, node: MyTreeNode) -> Union[MyTreeNode, None]:
         pass
 
-    def traverse(self) -> None:
-        pass
+    def traverse_in_order(self, tree_node: MyTreeNode) -> None:
+        # in-order traversing
+        if tree_node is None:
+            return
+        self.traverse_in_order(tree_node.left)
+        print(tree_node.value)
+        self.traverse_in_order(tree_node.right)
+
+    def traverse_preorder(self, tree_node: MyTreeNode):
+        if tree_node is None:
+            return
+        print(tree_node.value)
+        self.traverse_in_order(tree_node.left)
+        self.traverse_in_order(tree_node.right)
+
+    def traverse_postorder(self, tree_node: MyTreeNode):
+        if tree_node is None:
+            return
+        self.traverse_in_order(tree_node.left)
+        self.traverse_in_order(tree_node.right)
+        print(tree_node.value)
 
 
-root = MyTreeNode(1)
-n1 = MyTreeNode(5)
-n2 = MyTreeNode(3)
-n3 = MyTreeNode(10)
-n4 = MyTreeNode(6)
+root = MyTreeNode(55)
+n1 = MyTreeNode(25)
+n2 = MyTreeNode(17)
+n3 = MyTreeNode(11)
+n4 = MyTreeNode(33)
+n5 = MyTreeNode(61)
+n6 = MyTreeNode(89)
+n7 = MyTreeNode(30)
+n8 = MyTreeNode(40)
+n9 = MyTreeNode(56)
+n10 = MyTreeNode(82)
+n11 = MyTreeNode(95)
+
 tree = MyBinarySearchTree()
 tree.insert(root, n1)
 tree.insert(root, n2)
 tree.insert(root, n3)
 tree.insert(root, n4)
-print(tree.search(root, n4.value).value)
+tree.insert(root, n5)
+tree.insert(root, n6)
+tree.insert(root, n7)
+tree.insert(root, n8)
+tree.insert(root, n9)
+tree.insert(root, n10)
+tree.insert(root, n11)
+print("In order")
+print(tree.traverse_in_order(root))
+print("Pre order")
+print(tree.traverse_preorder(root))
+print("Post order")
+print(tree.traverse_postorder(root))
