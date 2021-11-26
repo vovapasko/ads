57a7ce23e9563131e87b7e5aca01cc583a7a162e
vovapasko <vovapasko1699@gmail.com>
2021-11-26 15:43:05 +0100

    made a template for MyHeap

---
+++ my_data_structures/my_heap.py
@@ -0,0 +1,18 @@
+class MyHeap:
+    def __init__(self) -> None:
+        pass
+
+    def remove(self):
+        pass
+
+    def insert(self):
+        pass
+
+    def get_parent(self, node: int):
+        pass
+
+    def get_left_child(self, node: int):
+        pass
+
+    def get_right_child(self, node: int):
+        pass
