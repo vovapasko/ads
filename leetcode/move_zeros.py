57c98f8e325ca4aaafe387ba476c46ec74a37cc1
vovapasko <vovapasko1699@gmail.com>
2021-12-11 13:42:14 +0100

    Completed move zeroes

--- leetcode/move_zeros.py
+++ leetcode/move_zeros.py
@@ -1,9 +1,16 @@
 from typing import List
 
 
-def moveZeroes(nums: List[int]) -> None:
-    if len(nums) == 1:
-        return
-    for i in range(len(nums) - 1):
-        if nums[i] == 0:
-            nums[i], nums[i + 1] = nums[i + 1], nums[i]
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        non_zeroes = []
+        for el in nums:
+            if el != 0:
+                non_zeroes.append(el)
+        for i in range(len(non_zeroes)):
+            nums[i] = non_zeroes[i]
+        for i in range(len(non_zeroes), len(nums)):
+            nums[i] = 0
