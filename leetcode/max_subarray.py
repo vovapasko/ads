bdad947f0e334baa0c30e7c817dd89a683e0023c
vovapasko <vovapasko1699@gmail.com>
2021-12-13 19:37:29 +0100

    Tried to solve max subarray

--- leetcode/max_subarray.py
+++ leetcode/max_subarray.py
@@ -1,30 +1,24 @@
-# Given an integer array nums, find the contiguous subarray 
+# Given an integer array nums, find the contiguous subarray
 # (containing at least one number)
 # which has the largest sum and return its sum.
 # A subarray is a contiguous part of an array.
 from typing import List
 
+
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        max_sums = self.maxSum(nums, [], {})
-        return max(max_sums)
-        
-    def maxSum(self, nums: list, sums: list, memo: dict) -> list:
-        if len(nums) == 1: 
-            sums.append(nums[0])
-            return sums
-        s = self.helper(nums, memo)
-        sums.append(s)
-        return self.maxSum(nums[1:], sums)
-    
-    def helper(self, arr):
-        tmp = 0
-        new_arr = []
-        for el in arr:
-            tmp += el
-            new_arr.append(tmp)
-        return max(new_arr)
+        sums = []
+        tmp_sum = 0
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                sums.append(tmp_sum)
+                tmp_sum = 0
+                continue
+            else:
+                tmp_sum += nums[i]
+        return max(sums)
+
 
 s = Solution()
-print(s.maxSubArray([-2, 3, 4, 5])) 
-print(s.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+print(s.maxSubArray([-2, 3, 4, 5]))
+print(s.maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
